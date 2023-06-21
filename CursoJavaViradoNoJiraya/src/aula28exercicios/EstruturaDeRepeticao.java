72409a81dd0cea4d82fd5a2ee2090e1f38203681
Lucas Eduardo Volkmann <lucas.volkmann2001@hotmail.com>
2023-06-20 21:10:47 -0300

    Resolvido o exercício da aula 28

---
+++ CursoJavaViradoNoJiraya/src/aula28exercicios/EstruturaDeRepeticao.java
@@ -0,0 +1,19 @@
+package aula28exercicios;
+
+public class EstruturaDeRepeticao {
+    
+    /*
+     * Imprima todos os pares de 0 até 1000000
+     * 
+     */
+    public static void main(String[] args) {
+        
+        for(int i=2;i<1000000;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+
+
+    }
+}

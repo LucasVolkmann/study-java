6e1c80bff373920571dbbd73c27eedddb34a5d16
Lucas Eduardo Volkmann <lucas.volkmann2001@hotmail.com>
2023-06-21 14:03:51 -0300

    Resolvido o exercicio da aula 30

---
+++ CursoJavaViradoNoJiraya/src/aula30exercicios/EstruturasDeRepeticaoBreak.java
@@ -0,0 +1,34 @@
+package aula30exercicios;
+
+import java.util.Scanner;
+
+public class EstruturasDeRepeticaoBreak {
+    
+    /*
+     Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
+     Condição: valorParcela >= 1000
+
+     -> Descobrir todas as maneiras de parcelamentos com parcelas que são maiores que 1000
+     */
+
+    public static void main(String[] args) {
+        
+        try (Scanner s = new Scanner(System.in)) {
+
+            double valor;
+            System.out.println("Qual o valor do carro?");
+            valor = s.nextDouble();
+
+            for (int parcela = 1; parcela <= valor; parcela++) {
+                double valorPArcela = valor / parcela;
+                if(valorPArcela >= 1000){
+                    System.out.println(parcela + "X  de R$"+valorPArcela);
+                }
+            }
+
+
+
+        }
+
+    }
+}
